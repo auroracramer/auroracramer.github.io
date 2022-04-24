2119ca462229898b15716f6951c4ef8c6e196061
Aurora Cramer <aurora.linh.cramer@gmail.com>
2022-04-24 11:55:42 -0400

    Implement pronoun stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,47 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
+//
+
+const pronouns = {
+    subject: ["she", "they"],
+    object: ["her", "them"],
+    determiner: ["her", "their"],
+    possesive: ["hers", "theirs"],
+    reflexive: ["herself", "themself"]
+}
+
+function randomPronouns(form) {
+    let r = Math.random();
+    let pns = pronouns[form];
+    let num_pns = pns.length;
+    return pns[Math.floor(r * num_pns)];
+}
+
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+function renderPronouns() {
+    const elements = document.getElementsByClassName("pronoun");
+    elements.forEach(function (el, idx) {
+        let form = el.getAttribute('form');
+        let case_ = el.getAttribute('case')
+        let pn = randomPronouns(form);
+
+        switch (case_) {
+            case "capital":
+                pn = capitalizeFirstLetter(pn);
+                break;
+            case "upper":
+                pn = pn.toUpperCase();
+                break;
+            case "lower":
+            default:
+                break;
+        }
+
+        el.innerHTML = pn;
+    });
+}
